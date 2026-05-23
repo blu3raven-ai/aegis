64dd31e3659decbea215a5e1bb813437b3d22d27
Gregory Tan <80577604+Grg0rry@users.noreply.github.com>
2026-05-23 22:40:12 +0800

    feat: add shared FindingDrawer primitives and make shell flex-col for sticky footer

---
+++ components/shared/FindingDrawer/index.ts
@@ -0,0 +1,7 @@
+// components/shared/FindingDrawer/index.ts
+export { DrawerHeader } from "./DrawerHeader"
+export { DrawerStatusBanner } from "./DrawerStatusBanner"
+export { DrawerSection } from "./DrawerSection"
+export { DrawerCodeBlock } from "./DrawerCodeBlock"
+export { DrawerDetailGrid } from "./DrawerDetailGrid"
+export { DrawerFooter } from "./DrawerFooter"
